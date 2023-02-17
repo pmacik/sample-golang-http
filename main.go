41d2e2e600dd7e80fe589dc3806fe9a27e44e1b1
Pavel Macík <pavel.macik@gmail.com>
2023-02-17 15:15:37 +0100

    Fix name detector
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("Hello, %s!", r.URL.Path[7:])
+	message := fmt.Sprintf("Hello, %s!", r.URL.Path[1:])
 	fmt.Println(message)
 	fmt.Fprintf(w, "%s", message)
 }
